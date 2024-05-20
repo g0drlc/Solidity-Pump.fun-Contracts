cd6b94d30d75bc28c97c0881c558f8ff4c201cb3
g0drlc <utommy.abs@gmail.com>
2024-05-20 23:21:08 +0100

    first commit

--- ignition/modules/PumpFun.js
+++ ignition/modules/PumpFun.js
@@ -1,7 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("LockModule", (m) => {
-  const pump_fun = m.contract("PumpFun", ["0xd8B138cda3e5fE3E54735c9d5B1830b3e913Be3b", "0x4Da17A81618C99817B0Dd96898ED07bA54e3B18a", "0x6627f8ddc81057368F9717042E38E3DEcb68dAc3", 5, 5, 5]);
+  const pump_fun = m.contract("PumpFun", ["0x30F403646134F53A5E31a4082b96b3F1e76e8745", "0x9Bd79d55bDfb5d816A2F2D0B5a93526C084c8654", "0x6627f8ddc81057368F9717042E38E3DEcb68dAc3", 5, 5, 5]);
 
   return { pump_fun };
 });
--- ignition/modules/Router.js
+++ ignition/modules/Router.js
@@ -1,7 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("LockModule", (m) => {
-  const router = m.contract("Router", ["0xd8B138cda3e5fE3E54735c9d5B1830b3e913Be3b", "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9"]);
+  const router = m.contract("Router", ["0x30F403646134F53A5E31a4082b96b3F1e76e8745", "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9"]);
 
   return { router };
 });
